41828e08f717eb963019b2088680fe9a9f676df9
Tomas Staškevičius <akurat2012@gmail.com>
2025-01-01 21:14:23 +0200

    first commit

---
+++ modules/base_generator.py
@@ -0,0 +1,22 @@
+import os
+
+
+class BaseGenerator:
+    def __init__(self, project_folder):
+        # Forming paths to the project directories
+        self.project_folder = os.path.join('projects', project_folder)
+
+        self.generated_images = os.path.join(self.project_folder, 'generated_images')
+        self.generated_video = os.path.join(self.project_folder, 'generated_video')
+
+        # Automatically create project folder and its subdirectories if they do not exist
+        os.makedirs(self.generated_images, exist_ok=True)
+        os.makedirs(self.generated_video, exist_ok=True)
+
+    def read_csv(self, file_name):
+        # Logic for reading a CSV file
+        pass
+
+    def write_csv(self, file_name, data):
+        # Logic for writing data to a CSV file
+        pass
---
+++ modules/voice_generator.py
@@ -0,0 +1,10 @@
+from modules.base_generator import BaseGenerator
+
+
+class VoiceGenerator(BaseGenerator):
+    def __init__(self, project_folder):
+        # Call the constructor of the base class
+        super().__init__(project_folder)
+
+    def execute(self):
+        pass

9676f176f46f250e052b8ddf4383146a1f5b52b0
Tomas Staškevičius <akurat2012@gmail.com>
2025-01-15 21:51:11 +0200

    smooth clip transitions

--- modules/video_generator.py
+++ modules/video_generator.py
@@ -1,7 +1,7 @@
 import os
 from modules.base_generator import BaseGenerator
 from moviepy.editor import AudioFileClip
-from moviepy.editor import ImageClip, VideoFileClip, concatenate_videoclips
+from moviepy.editor import ImageClip, VideoFileClip, concatenate_videoclips, vfx
 
 
 class VideoGenerator(BaseGenerator):
@@ -17,6 +17,7 @@ class VideoGenerator(BaseGenerator):
 
     def execute(self, video_dict, media_paths_dict):
         clip_paths = []
+        media_types = []
 
         for scene in video_dict['scenes']:
             for path in media_paths_dict:
@@ -50,6 +51,13 @@ class VideoGenerator(BaseGenerator):
                 video_clip.write_videofile(video_path, codec="libx264", fps=24)
                 video_clip.close()
                 audio_clip.close()
+
+                if media_path.lower().endswith(('.mp4', '.mov', '.avi')):
+                    media_types.append("video")
+                else:
+                    media_types.append("image")
+            else:
+                media_types.append("video")
 
             clip_paths.append(video_path)
 
@@ -85,14 +93,21 @@ class VideoGenerator(BaseGenerator):
         
     def concatenate_video_clips(self, clip_paths, final_video_path):
         # Concatenate the video clips
-        video_clips = [VideoFileClip(clip_path) for clip_path in clip_paths]
+        video_clips = [VideoFileClip(clip_path).subclip(0, VideoFileClip(clip_path).duration - 0.1) for clip_path in clip_paths]
         
         # Resize all clips to the same size
         min_width = min(clip.size[0] for clip in video_clips)
         min_height = min(clip.size[1] for clip in video_clips)
         video_clips = [clip.resize(newsize=(min_width, min_height)) for clip in video_clips]
         
-        final_video = concatenate_videoclips(video_clips)
+        # Apply slide-in animation
+        # Apply fade-in and fade-out animation
+        for i in range(len(video_clips) - 1):
+            video_clips[i] = video_clips[i].fx(vfx.fadeout, duration=0.2)
+            video_clips[i + 1] = video_clips[i + 1].fx(vfx.fadein, duration=0.2)
+            print("Fade-in and fade-out animation applied")
+        
+        final_video = concatenate_videoclips(video_clips, method="compose")
         final_video.write_videofile(final_video_path, codec="libx264", fps=24)
         final_video.close()
         return final_video_path
